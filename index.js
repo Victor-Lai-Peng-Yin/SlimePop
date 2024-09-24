ab46eb536dda9eb782db8d4727827ba2484ec25e
Victor Lai Peng Yin <91249309+Victor-Lai-Peng-Yin@users.noreply.github.com>
2024-09-24 20:19:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function () {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("ServiceWorker.js");
-    }
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("ServiceWorker.js");
+  }
 });
-  
+
 var unityInstanceRef;
 var unsubscribe;
 var container = document.querySelector("#unity-container");
@@ -13,42 +13,42 @@ var progressBarFull = document.querySelector("#unity-progress-bar-full");
 var warningBanner = document.querySelector("#unity-warning");
 
 function unityShowBanner(msg, type) {
-  function updateBannerVisibility() {
-    warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
-  }
-  var div = document.createElement('div');
-  div.innerHTML = msg;
-  warningBanner.appendChild(div);
-  if (type == 'error') div.style = 'background: red; padding: 10px;';
-  else {
-    if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
-    setTimeout(function() {
-      warningBanner.removeChild(div);
-      updateBannerVisibility();
-    }, 5000);
-  }
-  updateBannerVisibility();
+function updateBannerVisibility() {
+  warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+}
+var div = document.createElement('div');
+div.innerHTML = msg;
+warningBanner.appendChild(div);
+if (type == 'error') div.style = 'background: red; padding: 10px;';
+else {
+  if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
+  setTimeout(function() {
+    warningBanner.removeChild(div);
+    updateBannerVisibility();
+  }, 5000);
+}
+updateBannerVisibility();
 }
 
 var buildUrl = "Build";
 var loaderUrl = buildUrl + "/Build.loader.js";
 var config = {
-  dataUrl: buildUrl + "/Build.data.unityweb",
-  frameworkUrl: buildUrl + "/Build.framework.js.unityweb",
-  codeUrl: buildUrl + "/Build.wasm.unityweb",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "DefaultCompany",
-  productName: "Unityproject",
-  productVersion: "1.0",
-  showBanner: unityShowBanner,
+dataUrl: buildUrl + "/Build.data.unityweb",
+frameworkUrl: buildUrl + "/Build.framework.js.unityweb",
+codeUrl: buildUrl + "/Build.wasm.unityweb",
+streamingAssetsUrl: "StreamingAssets",
+companyName: "DefaultCompany",
+productName: "Unityproject",
+productVersion: "1.0",
+showBanner: unityShowBanner,
 };
 
 // 手機設備設定
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-  var meta = document.createElement('meta');
-  meta.name = 'viewport';
-  meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-  document.getElementsByTagName('head')[0].appendChild(meta);
+var meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+document.getElementsByTagName('head')[0].appendChild(meta);
 }
 
 loadingBar.style.display = "block";
@@ -57,32 +57,33 @@ loadingBar.style.display = "block";
 var script = document.createElement("script");
 script.src = loaderUrl;
 script.onload = () => {
-  createUnityInstance(canvas, config, (progress) => {
-    progressBarFull.style.width = 100 * progress + "%";
-  }).then((unityInstance) => {
-    unityInstanceRef = unityInstance;
-    loadingBar.style.display = "none";
+createUnityInstance(canvas, config, (progress) => {
+  progressBarFull.style.width = 100 * progress + "%";
+}).then((unityInstance) => {
+  unityInstanceRef = unityInstance;
+  loadingBar.style.display = "none";
 
-    // 當 Unity 加載完成後，將 Telegram 的 initData 傳給 Unity
-    Telegram.WebApp.ready();
-    var initData = Telegram.WebApp.initData;
-    console.log("initData is " + initData);
-    SendAuthDataToUnity(initData);
-    
-  }).catch((message) => {
-    alert(message);
-  });
+  // 當 Unity 加載完成後，將 Telegram 的 initData 傳給 Unity
+  Telegram.WebApp.ready();
+  var initData = Telegram.WebApp.initData;
+  console.log("initData is " + initData);
+  SendAuthDataToUnity(initData);
+  
+}).catch((message) => {
+  alert(message);
+});
 };
 document.body.appendChild(script);
 
 function SendAuthDataToUnity(initData) {
-  if (unityInstanceRef) {
-    // 傳遞數據給 Unity C# 函數
-    unityInstanceRef.SendMessage('JsonObject', 'ReceiveInitData', initData);
-  } else {
-    console.error("Unity instance not ready");
-  }
+if (unityInstanceRef) {
+  // 傳遞數據給 Unity C# 函數
+  unityInstanceRef.SendMessage('JsonObject', 'ReceiveInitData', initData);
+} else {
+  console.error("Unity instance not ready");
 }
+}
+
 
 
 
